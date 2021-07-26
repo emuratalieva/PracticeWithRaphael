0360d570c576bc55edf739097ea4bcdc020676bc
emuratalieva <emuratalieva@gmail.com>
2021-07-26 17:34:20 -0400

    3rd commit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         // more codes here
+        // more lines of new codes here
+        // hello b 23
+
     }
 }
